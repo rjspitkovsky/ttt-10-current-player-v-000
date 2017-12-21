0fe4355a9579a49007be522dcd11e764707ac4b5
Richard Spitkovsky <rjspitkovsky@gmail.com>
2017-12-21 22:33:32 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -11,5 +11,9 @@ end
 def current_player(board)
   if turn_count(board) == 0
     return "X"
+  elsif turn_count(board) == 1
+    return "O"
+  elsif turn_count(board) % 2 == 0
+    return "X"
   end
 end
